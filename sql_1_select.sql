f5a6cccd4c47e9c63f1c1818e5eebe361b29e832
Mohamad Nour Shich Khalil <mohmad200.mm16@gmail.com>
2025-12-11 12:50:05 +0100

    finish

--- sql_1_select.sql
+++ sql_1_select.sql
@@ -16,58 +16,121 @@
 #
 # 1: Get a query to return "Hello World", 123
 # (Hint: 1 row, 2 columns)
-#
+
+USE world;
+
+select "hello world", 123 as message;
+
+
+
+
 SELECT "hello world",123;
 #
 # 2: Get everything from the city table
 # (Hint: Many many rows)
-#
+
+select * from City;
+
+
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
-#
+
+select * from City
+where district = 'aceh';
+
+
 #
 # 4: Get only the name of the cities where the countrycode is "bfa"
-#
-#
+
+select name from city
+ where countrycode = 'BFA';
+
+
 # 5: Get both the name and district of the cities where the countrycode is "tto"
-#
-#
+
+select name, district AS nm from city
+ where countrycode = 'tto';
+
+
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
-#
-#
+
+
+select districk AS nm from city
+where countrycode = 'arm';
+
+
 # 7: Get the cities with a name that starts with "bor"
-#
-#
+
+
+select * from city
+where substring(name,1,3) = 'bor';
+
+
 # 8: Get the cities with a name that contains the string "orto"
-#
-#
+
+select * from city
+where name like ('orto');
+
+
 # 9: Get the cities that has a population below 1000
-#
-#
+
+select * from city
+where population < 1000;
+
+
 # 10: Get the unique countrycodes from the cities that has a population below 1000
-#
-#
+
+
+select distinct countrycode from city
+ where population <1000;
+
+
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
-#
-#
+
+select * from city
+where countrycode = 'UKR' and population > 1000000;
+
+
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
-#
-#
+
+select * from city
+where population < 200 or population >9500000;
+
+
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
-#
-#
+
+select * from city
+where countryCode in ('TJK', 'MRT', 'AND', 'SJM');
+
+
+
 # 14: Get the cities with a population between 200 and 700 inclusive
-#
-#
+
+SELECT * FROM City
+WHERE Population >= 200 AND Population <= 700;
+
+
 # 15: Get the countries with a population between 8000 and 20000 inclusive
-#
-#
+
+select * from country
+where population between 8000 and 20000;
+
+
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
-#
-#
+
+select Name FROM Country
+WHERE IndepYear < 0;
+
+
 # 17: Get the countries that has no recorded independence year and a population above 1000000
-#
-#
+
+select * from country
+where indepyear is null and population > 1000000;
+
+
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
-#
+
+select * from country
+where surfacearea < 10 and lifeexpectancy is not null;
+
--- sql_2_join.sql
+++ sql_2_join.sql
@@ -3,61 +3,112 @@
 
 #
 # 1: Get the cities with a name starting with ping sorted by their population with the least populated cities first
-#
-#
+
+
+Select * from city
+where name like 'ping%' order by population ASC;
+
 # 2: Get the cities with a name starting with ran sorted by their population with the most populated cities first
-#
-#
+
+select * from city
+where name like 'ran%' order by population desc;
+
 # 3: Count all cities
-#
-#
+
+select count(*) AS all_cities from city;
+
 # 4: Get the average population of all cities
-#
-#
+
+select avg(population)
+AS average_population from city;
+
 # 5: Get the biggest population found in any of the cities
-#
-#
+
+select max(population)
+as max_population from city;
+
 # 6: Get the smallest population found in any of the cities
-#
-#
+
+select min(population)
+as min_population from city;
+
 # 7: Sum the population of all cities with a population below 10000
-#
-#
+
+select sum(population)
+as total_population_below_10000 from city
+where population < 10000;
+
 # 8: Count the cities with the countrycodes MOZ and VNM
-#
-#
+
+select count(*)
+as city_count from city
+where countrycode IN ('MOZ', 'VNM');
+
 # 9: Get individual count of cities for the countrycodes MOZ and VNM
-#
-#
+
+select countrycode, count(*)
+as city_count from city
+where countrycode IN ('MOZ', 'VNM') group by countrycode;
+
 # 10: Get average population of cities in MOZ and VNM
-#
-#
+
+select countrycode, avg(population)
+as average_population from city
+where countrycode in ('MOZ', 'VNM') group by countrycode;
+
 # 11: Get the countrycodes with more than 200 cities
-#
-#
+
+select countrycode, count(*)
+as city_count from city group by countrycode having count(*) > 200;
+
 # 12: Get the countrycodes with more than 200 cities ordered by city count
-#
-#
+
+select countrycode, count(*)
+as city_count from city group by countrycode having count(*) > 200 order by city_count desc;
+
 # 13: What language(s) is spoken in the city with a population between 400 and 500 ?
-#
-#
+
+select distinct cl.language from city c join countrylanguage cl on c.countrycode = cl.countrycode
+where c.population between 400 and 500;
+
 # 14: What are the name(s) of the cities with a population between 500 and 600 people and the language(s) spoken in them
-#
-#
+
+select c.name as city, cl.language from city c
+join countrylanguage cl on c.countrycode = cl.countrycode
+where c.population between 500 and 600;
+
 # 15: What names of the cities are in the same country as the city with a population of 122199 (including the that city itself)
-#
-#
+
+select c2.Name from city c1
+join city c2 on c1.countrycode = c2.countrycode
+where c1.population = 122199;
+
 # 16: What names of the cities are in the same country as the city with a population of 122199 (excluding the that city itself)
-#
-#
+
+select c2.Name from City c1
+join city c2 ON c1.countrycode = c2.countrycode
+where c1.population = 122199 and c2.population <> 122199;
+
 # 17: What are the city names in the country where Luanda is capital?
-#
-#
+
+select c.name from city c
+where c.countrycode = (select countrycode from city where name = 'Luanda');
+
 # 18: What are the names of the capital cities in countries in the same region as the city named Yaren
-#
-#
+
+select distinct cap.name as capital_name from city yaren
+join country cy on yaren.countrycode = cy.code
+join country creg on cy.region = creg.region
+join city cap on creg.capital = cap.ID where yaren.name = 'yaren';
+
 # 19: What unique languages are spoken in the countries in the same region as the city named Riga
-#
-#
+
+select distinct cl.language from city riga
+join country cr on riga.countrycode = cr.code
+join country creg on cr.region = creg.region
+join countrylanguage cl on creg.code = cl.countrycode where riga.name = 'Riga';
+
 # 20: Get the name of the most populous city
-#
+
+select name from city order by population desc limit 1;
+
